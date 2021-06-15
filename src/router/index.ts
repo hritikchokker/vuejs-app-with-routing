b59bf69a76c1c5d237648c114c73ddaa249435e5
hritikchokker <hchokker7@gmail.com>
2021-06-16 01:07:38 +0530

    dynamic routing done

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,19 +26,58 @@ const routes: Array<RouteConfig> = [
     name: "messageFeed",
     component: () =>
       import(/* webpackChunkName: "messages" */ "../views/MessageFeed.vue"),
+    props: true,
+    async beforeEnter(to: any, from: any, next: any) {
+      if (!to.params || !to.params.messages) {
+        const module = await import(
+          /* webpackChunkName: "messagesFeed" */ "../assets/messages"
+        );
+        const messages = module.default;
+        if (messages && messages.length > 0) {
+          to.params.messages = messages;
+        }
+      }
+
+      next();
+    },
   },
   {
-    path: "/message",
+    path: "/message/:id",
     name: "message",
     component: () =>
       import(/* webpackChunkName: "message" */ "../views/Message.vue"),
+    async beforeEnter(to: any, from: any, next: any) {
+      if (to.params && to.params.id) {
+        const id = to.params.id;
+        const module = await import(
+          /* webpackChunkName: "messagesFeed" */ "../assets/messages"
+        );
+        const messages = module.default;
+        if (messages && messages.length > 0 && id < messages.length) {
+          to.params.content = messages[id];
+        }
+      }
+      next();
+    },
     props: true,
   },
+  // {
+  //   path: "/message",
+  //   name: "message",
+  //   component: () =>
+  //     import(/* webpackChunkName: "message" */ "../views/Message.vue"),
+  //   props: true,
+  // },
   {
     path: "/error",
     name: "error",
     component: () =>
       import(/* webpackChunkName: "error" */ "../views/Error.vue"),
+  },
+  {
+    path: "*",
+    name: "404",
+    component: () => import(/* webpackChunkName: "404" */ "../views/404.vue"),
   },
 ];
 
